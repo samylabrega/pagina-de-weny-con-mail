8511dd05344ccba168cb45d0584a9b009d7315d2
samylabrega <161665313+samylabrega@users.noreply.github.com>
2024-12-03 01:53:16 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -9,8 +9,3 @@ document.querySelectorAll('nav a').forEach(anchor => {
     });
 });
 
-// Función para simular envío de formulario
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Gracias por tu mensaje. Me pondré en contacto contigo pronto.');
-});
